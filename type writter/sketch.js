cc9a86642ffbf6df5c775f4e04747875a57e6f95
seojinmoon <fastturtle12@snu.ac.kr>
2025-10-20 13:54:45 +0900

    added tw

---
+++ type writter/sketch.js
@@ -0,0 +1,23 @@
+function preload() {
+  img = loadImage('tw.png');
+}
+
+function setup() {
+  let c = createCanvas(windowWidth, windowHeight);
+  //pixelDensity(1);
+  rectMode(CORNER);
+  noStroke();
+  c.elt.style.position = 'fixed';
+  c.elt.style.top = '0';
+  c.elt.style.left = '0';
+  c.elt.style.zIndex = '9999';
+  c.elt.style.pointerEvents = 'none';
+  c.elt.style.backgroundColor = 'transparent';
+
+  document.documentElement.style.overflow = 'hidden';
+
+  clear();
+
+  // Draw the image.
+  image(img, 0, 0, 1800, 900);
+}
